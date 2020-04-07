2696c80affd20c9e2955f93c9e56381c9cb3b9f2
Trevor McKay <tmckay@redhat.com>
2020-04-07 11:50:43 -0400

    See if this works

---
+++ middleware/middleware-quarkus/getting-started/set-env.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+cd projects
+mkdir -p quarkus
+cd quarkus
+export GRAALVM_HOME=/usr/local/graalvm-ce-19.2.1
+echo 'export GRAALVM_HOME=/usr/local/graalvm-ce-19.2.1' >> ~/.bashrc
+clear
---
+++ servicemesh/8-egress/set-env.sh
@@ -0,0 +1,2 @@
+/usr/local/bin/launch.sh
+export PATH=$PATH:/root/installation/istio-1.0.5/bin/
